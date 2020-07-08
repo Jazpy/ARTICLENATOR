2bad7239c1018f1bf20f83a565c0a053c090d076
Jazpy <jaz.medtre@gmail.com>
2020-07-08 12:27:12 -0500

    Article relationships

--- src/analyze.py
+++ src/analyze.py
@@ -1,7 +1,9 @@
 import sys
+import string
 import os
 import re
 import snorkel
+import nltk
 import pandas as pd
 
 import labelers
@@ -87,10 +89,80 @@ def main():
   except RuntimeError as e:
     print(e, file=sys.stderr)
 
-  # Print predictions
-  for prediction, sentence, article in \
-    zip(predictions, all_sentences['sentence'], all_sentences['article']):
+  # Add predictions to article
+  for prediction, article in zip(predictions, all_sentences['article']):
     article.add_prediction(prediction)
+
+  # Get most used terms for CLASS predictions
+  all_dicts     = []
+  filtered_tags = ('DT', 'IN', 'CC', 'EX', 'TO', 'WDT', 'PRP',
+                   'VBG', 'CD', 'WRB', 'MD', 'VBZ', 'RP', 'SYM',
+                   'UH', 'PRP', 'PRP$', 'RB', 'RBS', 'WP', 'VB')
+  for article in articles:
+    curr_dict = {}
+
+    for sentence, prediction in \
+    zip(article.get_sentences(), article.get_predictions()):
+      if prediction != 0:
+        continue
+
+      filtered_sentence = re.sub(r'[^\w\s]', '', sentence.lower(), re.UNICODE)
+      toks = nltk.word_tokenize(filtered_sentence)
+      tags = [x[1] for x in nltk.pos_tag(toks)]
+
+      # Walk over sentence with two word sliding window
+      for i in range(len(toks) - 1):
+        w0           = toks[i]
+        w1           = toks[i + 1]
+        compound     = f'{w0} {w1}'
+
+        # Skip over useless tags
+        if (tags[i] in filtered_tags or
+            (len(w0) == 1 and w0 in string.punctuation)):
+          continue
+
+        # Add single word
+        count         = curr_dict.get(w0, 0) + 1
+        curr_dict[w0] = count
+
+        # Skip over useless tags
+        if (tags[i + 1] in filtered_tags or
+            (len(w1) == 1 and w1 in string.punctuation)):
+          continue
+
+        # Add two words
+        count               = curr_dict.get(compound, 0) + 1
+        curr_dict[compound] = count
+
+    all_dicts.append((article, curr_dict))
+
+  # Merge dictionaries
+  main_dict = {}
+  for article, d in all_dicts:
+    for key in d.keys():
+      main_get   = main_dict.get(key, (0, list()))
+      main_count = main_get[0] + 1
+      main_list  = main_get[1]
+      main_list.append(article)
+
+      main_dict[key] = (main_count, main_list)
+
+  # Filter low freqs and sort by freq
+  main_dict = dict(filter(lambda x: x[1][0] > 2, main_dict.items()))
+  main_dict_ordered_keys = sorted(main_dict.keys(),
+                                  key=lambda x: main_dict.get(x)[0],
+                                  reverse=True)
+
+  for i, k in enumerate(main_dict_ordered_keys[:31]):
+    print(f'{i}\t- {k}, {main_dict.get(k)[0]}')
+
+  while True:
+    choice = int(input('Choice: '))
+    if choice == -1:
+      break
+
+    for a in main_dict.get(main_dict_ordered_keys[choice])[1]:
+      print(a.get_name())
 
 if __name__ == "__main__":
   main()
